1adbb9e391707930df66847a1c401ab9ce08efcc
chandragie <chandragie@gmail.com>
2021-03-08 21:55:59 +0700

    Add qwitter project

---
+++ qwitter/qwitter/src/layouts/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          @click="left = !left"
+          class="mobile-only"
+        />
+
+        <q-toolbar-title class="text-weight-bold"> Qwitter </q-toolbar-title>
+        <q-icon name="auto_awesome" color="primary" size="md" />
+
+        <!-- <q-btn dense flat round icon="menu" @click="right = !right" /> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+      <!-- drawer content -->
+      <q-scroll-area></q-scroll-area>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      left: false,
+      right: false,
+    };
+  },
+};
+</script>
